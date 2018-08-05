529cb92184225ec001eb02c31e9cc935b92753e7
Karthik <springcoding@gmail.com>
2018-08-05 16:31:19 -0400

    Karthik's first commit

---
+++ src/main/java/com/mapledream/travelplan/TravelPlanApplication.java
@@ -0,0 +1,12 @@
+package com.mapledream.travelplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlanApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mapledream/travelplan/TravelplanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mapledream.travelplan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelplanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
